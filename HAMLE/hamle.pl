1df1b324a6046608acdc1493043caf106244d033
sergiomieic <sergio.mieic@gmail.com>
2015-11-23 21:02:54 +0000

    project creation with some notes

---
+++ HAMLE/hamle.pl
@@ -0,0 +1,21 @@
+%geracao tabuleiro inicial (a resolver)
+
+%criar relacao tamanho -> numero de pecas 
+%gerar uma posicao do tabuleiro aleatoria
+%escolher uma das direcoes, calcular distancia à margem e definir essa posicao com esse tamanho e como posicao ocupada
+%fazer o mesmo para o resto das pecas
+
+%usar predicado length para gerar lista de variaveis n/inicializadas ?
+
+
+
+%1º passo
+%-> resolver
+
+%verificar células brancas interconectadas 
+%começar na primeira branca, ir guardando velulas visitadas e tentar visitar todas as celulas brancas adjacente
+%se no finaol o numero de celulas visitadas for !=  do numero de cels bracnas é pq nao existe
+
+%verificar celulas pretas nao adjacentes
+%-basicamente é aplicar o mesmo predicado que nas celulas brancas
+%-mas verificar se o numero de celulas pretas interconectadas é 0
