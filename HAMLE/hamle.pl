550b4667acceb6c0db414d6cddad032a66878dd2
sergiomieic <sergio.mieic@gmail.com>
2015-12-10 22:47:25 +0000

    Redoing - adjoins free implementation

--- HAMLE/hamle.pl
+++ HAMLE/hamle.pl
@@ -16,6 +16,7 @@
 
 main(TabIni,Tamanho):-
         write('Tabuleiro problema:'),nl,
+        write('Predicado Printtab está errado'), 
         print_tab(TabIni,Tamanho),             
         hamle_solver(TabIni, TabSol,Tamanho),
         write('Tabuleiro solução:'),nl,
@@ -33,17 +34,36 @@ hamle_solver(TabIni, Tamanho, Pieces):-
         %
         %devolve lista de listas de indices para tabling
         convert_to_index_pos_list(DomainList,Tamanho,DomainIndexList),   
-        write(DomainIndexList),nl,
+        %write(DomainIndexList),nl,
         %tamanho da lista
         length(IndexList,Size),             
         %
         length(Pieces,Size),        
-        %
+        %aplicacao de dominios
         do_tabling(Pieces,DomainIndexList),
-        
+        %
+        restrict_adjoins(Pieces,Tamanho),
+        %
         all_different(Pieces),
-        %write(Pieces),
-        labeling([],Pieces).
+        %
+        labeling([],Pieces),
+        write(Pieces).
+
+%criar no final forma de ver qual peca se moveu para tal sitio par anao haver confusoes de valores de pecas
+%usar a posicao inicial vs posiciao final
+
+restrict_adjoins([],_).
+restrict_adjoins([H|T],Size):-
+        restrict_adjoins(T,Size),
+        restrict_aux(H,T,Size).
+
+restrict_aux(_,[],_).
+restrict_aux(H,[H1|T1],Size):-
+        restrict_aux(H,T1,Size),
+        H #\= H1 + 1,
+        H #\= H1 - 1, 
+        H #\= H1 + Size,
+        H #\= H1 - Size.
 
 do_tabling([],[]).
 do_tabling([Hpiece|Tpiece],[Hdom|Tdom]):-
@@ -97,120 +117,7 @@ get_coo_down([X,Y],Peca,[X,NewY]):-
         NewY >= 1.  
         
 get_coo_down(_,_,[]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%CANCRO %%%%%%%%%%%%%%%%
-/*hamle_solver(TabIni, TabSol, Tamanho):-  
-        %      
-        % gera tab solucao com vars n/instanciadas
-        generate_tab(TabSol,Tamanho,Tamanho), !,
-        %
-        %lista de pecas do tab
-        get_pieces_list(TabIni,PList),
-        %
-        %lista de posicoes de pecas no tab
-        get_pieces_pos_list(TabIni,1, PosList),
-        %
-        %numero de pecas no tab
-        length(PList, NumPecas),
-        %
-        %generate list of directions for pieces with length = numPecas
-        length(DirList,NumPecas),        
-        %
-        %[1,4]: Num of directions
-        domain(DirList,1,4),
-        %
-        restrict_dirs(TabIni,TabSol,DirList,PList,PosList,Tamanho),
-        %
-        labeling([],DirList),  !.   % nao da mais solucoes
-*/                                
-
-%2º passo 
-%gerar tabuleiro 
-
-        
-        
-        
-        
-        
-%%%%%%%%%%%%%
-restrict_dirs(_,_,[],[],[],_).
-restrict_dirs(Tab,Tsol,[Hdir|Tdir],[Hpeca|Tpeca],[Hpos|Tpos], TabSize):-
-        %restringe dirs e obtem novas coos
-        get_coos_dir(Hpos,Hpeca,Hdir,NewX,NewY),     
-        %               
-        inside_bounds(NewX,NewY,TabSize),
-        %
-        % posicao tem de estar livre  
-        restrict_position(Tsol,NewX,NewY,Hpeca),
-        %         
-        % posicoes adjacentes têm de estar livres
-        adjoins_free(Tsol,TabSize,NewX,NewY),
-        %        
-        restrict_dirs(Tab,Tsol,Tdir,Tpeca,Tpos,TabSize).
-
-                
-adjoins_free(Tab,TabSize,X,Y):-
-        foreach(between(1,4,Dir), check_adjoin_dir(Tab,TabSize,X,Y,Dir)).
-
-                
-check_adjoin_dir(Tab,TabSize,X,Y,1):-
-        NewY is Y-1,
-        (outside_bounds(X,NewY,TabSize);
-        restrict_position(Tab,X,NewY,0),!).           
-
-check_adjoin_dir(Tab,TabSize, X,Y,2):-
-        NewY is Y+1,
-        (outside_bounds(X,NewY,TabSize);
-        restrict_position(Tab,X,NewY,0),!).
-
-check_adjoin_dir(Tab,TabSize,X,Y,3):-
-        NewX is X+1,
-        (outside_bounds(NewX,Y,TabSize);        
-        restrict_position(Tab,NewX,Y,0),!).
-
-check_adjoin_dir(Tab,TabSize,X,Y,4):-
-        NewX is X-1,
-        (outside_bounds(NewX,Y,TabSize);
-        restrict_position(Tab,NewX,Y,0),!).       
-
-%%%%
-restrict_position(Tab,X,Y,Val):-        
-        nth1(Y, Tab, Linha),
-        element(X, Linha, V),
-        write(V),nl,
-        write(Val),nl,!,
-        V #= Val.
-                
-                
-outside_bounds(X,Y,TabSize):-
-       X < 1 ;  X > TabSize;
-       Y < 1 ; Y > TabSize.        
-                
-inside_bounds(X,Y,TabSize):-
-       X > 0 ,  X =< TabSize,
-       Y > 0 , Y =< TabSize.
-        
-%1-norte,2-sul,3-este,4 oeste.        
-get_coos_dir([X,Y],ValPeca,Dir,NewX,NewY):-
-        Dir #= 1,
-        NewX is X,
-        NewY is Y - ValPeca.               
-                
-get_coos_dir([X,Y],ValPeca,Dir,NewX,NewY):-
-        Dir #= 2,
-        NewX is X,
-        NewY is Y + ValPeca. 
-        
-get_coos_dir([X,Y],ValPeca,Dir,NewX,NewY):-
-        Dir #= 3,
-        NewX is X + ValPeca,
-        NewY is Y. 
-        
-get_coos_dir([X,Y],ValPeca,Dir,NewX,NewY):-
-        Dir #= 4,
-        NewX is X - ValPeca,
-        NewY is Y.                         
+       
        
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
 %obter lista de posicoes 
@@ -222,19 +129,7 @@ get_pieces_pos_list([Htab|Ttab],Y,List):-
        append(List1,NewList,List).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-fill_tab([],_).
-fill_tab([H|T],Val):-
-       fill_list(H,Val),
-       fill_tab(T,Val). 
-
-fill_list([],_).
-fill_list([H|T],Val):-
-      H is Val,
-      fill_list(T,Val).  
-                
 matrix_get(Tab,X,Y,V):- nth1(Y,Tab,Linha),nth1(X,Linha,V).                                                                                     
 
 %gera matriz tamanho Size x Size
@@ -261,27 +156,6 @@ get_pieces_list_aux([H|T],List):-
 get_pieces_list_aux([_|T],L):-
         get_pieces_list_aux(T,L).
 %%%%%%%%%%%%%
-
-
-%retorna valor maximo de uma lista de listas
-get_max_value([],Max,Max).
-get_max_value([H|T],MaxActual,Max):-
-        get_max_value_aux(H,MaxActual,MaxList),
-        NextMax is max(MaxList,MaxActual),
-        get_max_value(T,NextMax,Max).
-
-get_max_value_aux([],Max,Max).
-get_max_value_aux([H|T],MaxActual,Max):-
-         NextMax is max(H,MaxActual),
-         get_max_value_aux(T,NextMax,Max).
-
-%efectua labeling a uma lista de listas
-do_labeling([]).
-do_labeling([H|T]):-
-        labeling([],H),
-        do_labeling(T).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 print_tab([],_).%:- print_char_n_times('_', Size).
 print_tab([H|T],Size):-
